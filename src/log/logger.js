8b3e5ea29cf2448e7c9484a9659ae87e106b2061
ortutay <marcell.ortutay@gmail.com>
2024-10-31 15:54:09 -0700

    log debug in browser env

--- src/log/logger.js
+++ src/log/logger.js
@@ -16,7 +16,7 @@ try {
   LOG_LEVEL = process.env.FETCHFOX_LOG_LEVEL || process.env.FF_LOG || 'warn';
 } catch (e) {
   // Likely browser env
-  LOG_LEVEL = 'info';
+  LOG_LEVEL = 'debug';
 }
 
 prefix.reg(log);
