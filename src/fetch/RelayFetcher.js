5a580a03d9c68248321c026902c7a84d8407009c
ortutay <marcell.ortutay@gmail.com>
2024-10-31 22:03:04 -0700

    close if none in flight

--- src/fetch/RelayFetcher.js
+++ src/fetch/RelayFetcher.js
@@ -6,11 +6,11 @@ import { BaseFetcher } from './BaseFetcher.js';
 
 export const RelayFetcher = class extends BaseFetcher {
   constructor(options) {
-    console.log('relay got options:', options);
-
     super(options);
     this.client = new Client(options?.host);
     this.relayId = options?.relayId;
+
+    this._inFlight = 0;
   }
 
   async _maybeInit() {
@@ -22,25 +22,33 @@ export const RelayFetcher = class extends BaseFetcher {
   async _fetch(url, options) {
     await this._maybeInit();
 
-    logger.debug(`Relay fetcher sending message expecting reply for ${url}`);
+    this._inFlight++;
 
-    const reply = await new Promise((ok) => {
-      this.client.send(
-        { command: 'fetch', url },
-        (r) => {
-          // console.log('relay fetcher got reply ->', r);
-          ok(r);
-        })});
+    try {
+      logger.debug(`Relay fetcher sending message expecting reply for ${url}`);
 
-    logger.debug(`Relay fetcher got reply: ${Object.keys(reply).join(', ')}`);
-    logger.info(`Relay fetcher response: "${(reply?.html || '').substr(0, 140).replace(/[\n\t ]+/g, ' ')} for ${url}`);
+      const reply = await new Promise((ok) => {
+        this.client.send(
+          { command: 'fetch', url },
+          ok
+        )});
 
-    const doc = new Document();
-    doc.loadData(reply);
+      logger.debug(`Relay fetcher got reply: ${Object.keys(reply).join(', ')}`);
+      logger.info(`Relay fetcher response: "${(reply?.html || '').substr(0, 140).replace(/[\n\t ]+/g, ' ')} for ${url}`);
 
-    logger.debug(`Relay fetcher loaded document: ${doc}`);
+      const doc = new Document();
+      doc.loadData(reply);
 
-    return doc;
+      logger.debug(`Relay fetcher loaded document: ${doc}`);
+
+      return doc;
+    } finally {
+      this._inFlight--;
+
+      if (this._inFlight == 0) {
+        await this.cleanup();
+      }
+    }
   }
 
   async cleanup() {
