0951f602fdd73ad75f63d85a65fd49caa0b9749c
ortutay <marcell.ortutay@gmail.com>
2024-11-04 10:42:16 -0800

    export stepDescriptions in web.js

--- src/web.js
+++ src/web.js
@@ -1,6 +1,6 @@
-console.log('Using FetchFox web exports');
 console.log('FetchFox is new! If you need help, ask on Discord: https://discord.gg/mM54bwdu59');
 
 export { webfox } from './webfox/index.js';
+export { stepDescriptions } from './step/info.js';
 export { Client } from './relay/Client.js';
 export { ChromeRelayActor } from './relay/ChromeRelayActor.js';
