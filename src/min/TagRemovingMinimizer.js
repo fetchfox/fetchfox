2f63cde905b09b1ae937e7b94879074025224282
ortutay <marcell.ortutay@gmail.com>
2024-11-20 22:31:39 -0800

    youtube.com special case

--- src/min/TagRemovingMinimizer.js
+++ src/min/TagRemovingMinimizer.js
@@ -11,12 +11,18 @@ export const TagRemovingMinimizer = class extends BaseMinimizer {
   }
 
   async _min(doc) {
-    logger.info(`Minimizing ${doc} with tag removing heuristics`);
+    let removeTags = this.removeTags;
+    if (doc.url.indexOf('youtube.com') != -1) {
+      logger.debug(`Not removing <script> on youtube.com`);
+      removeTags = removeTags.filter(t => t != 'script');
+    }
+
+    logger.info(`Minimizing ${doc} by removing tags: ${removeTags.join(', ')}`);
 
     let initial = (doc.html || '').replace(/[ \t\n]+/g, ' '); // remove extra whitespace
     const root = parse(initial);
 
-    this.removeTags.forEach(tag => {
+    removeTags.forEach(tag => {
       root.querySelectorAll(tag).forEach(element => {
         element.replaceWith('');
       });
