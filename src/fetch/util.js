2138e9dd06e4f6233fca4a2ce71708862a58dcca
ortutay <marcell.ortutay@gmail.com>
2024-12-05 21:33:33 -0800

    new test, fix log output

--- src/fetch/util.js
+++ src/fetch/util.js
@@ -3,7 +3,7 @@ import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 export const presignS3 = async ({ bucket, key, contentType, acl }) => {
-  logger.debug(`Generating presigned URL 2`);
+  logger.debug(`Generating presigned URL`);
   const s3 = new S3Client();
   const command = new PutObjectCommand({
     Bucket: bucket,
---
+++ test/cases/ownyourlabs.com.test.js
@@ -0,0 +1,27 @@
+import os from 'os';
+import fs from 'fs';
+import assert from 'assert';
+import process from 'node:process';
+import { fox } from '../../src/index.js';
+
+describe('news.ycombinator.com', function() {
+  this.timeout(5 * 60 * 1000);
+
+  it('should work @run', async () => {
+    let countPartials = 0;
+    const out = await fox
+      .init('https://ffcloud.s3.us-west-2.amazonaws.com/fetchfox-docs/jb5ym2yq47/https-ownyourlabs-com-shop-oyl-.html')
+      .extract({
+        product_name: 'What is the name of this product?',
+        price: 'What is the price of this product? Format: $XX.XX'
+      })
+      .limit(20)
+      .run();
+
+    console.log('out', out.items);
+    console.log('out', out.items.length);
+
+    assert.equal(out.items.length, 20);
+  });
+
+});
