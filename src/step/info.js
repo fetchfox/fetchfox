0b844fe5744c010745a47afd2d4a567ae02a83df
ortutay <marcell.ortutay@gmail.com>
2024-12-13 15:19:58 -0800

    max pages for fetch

--- src/step/info.js
+++ src/step/info.js
@@ -21,7 +21,7 @@ const combineInfo = (info) => {
     required: false,
   };
 
-  if (['const', 'extract', 'crawl'].includes(info.name)) {
+  if (['const', 'extract', 'crawl', 'fetch'].includes(info.name)) {
     combined.args.maxPages = {
       description: 'Max number of pages to fetch from source URLs',
       format: 'number',
