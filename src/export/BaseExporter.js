76c96321df41340fceb9b62da22d64965589127d
ortutay <marcell.ortutay@gmail.com>
2024-12-26 21:03:24 -0800

    remove more unused code

--- src/export/BaseExporter.js
+++
@@ -1,9 +0,0 @@
-export const BaseExporter = class {
-  constructor(options) {
-    this.format = options?.format || 'jsonl';
-
-    // TODO: options for:
-    // - append vs. truncate
-    // - combine vs. single item out
-  }
-}
--- src/export/index.js
+++
@@ -1,6 +0,0 @@
-import { logger } from '../log/logger.js';
-import { Exporter } from './Exporter.js';
-
-export const getExporter = (which, options) => {
-  return new Exporter({ ...options, destination: which });
-}
