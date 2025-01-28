9a4f647a3ab801399bc968cf665e356463f66dee
Sanchit Dass <38399292+sanchitd9@users.noreply.github.com>
2025-01-28 00:04:45 -0500

    Add GMaps benchmark

---
+++ benchmark/paginate/google.com-maps-search-paginate.bench.js
@@ -0,0 +1,24 @@
+import { fox } from '../../src/index.js';
+import { itRunMatrix } from '../lib/index.js';
+import { standardMatrix } from '../lib/matrix.js';
+import { checkIncreasingSize } from '../lib/checks.js';
+
+describe('paginate google.com maps restaurants search', async function() {
+  const matrix = standardMatrix();
+
+  const wf = await fox
+    .init('https://www.google.com/maps/search/Restaurants/@42.3233141,-71.162825,14z/')
+    .fetch({ maxPages: 5 })
+    .plan();
+
+  return itRunMatrix(
+    it,
+    'paginate google.com maps restaurants search',
+    wf.dump(),
+    matrix,
+    [
+      checkIncreasingSize,
+    ],
+    { shouldSave: true }
+  );
+});
