f131aab1d0a0837f646cce8c2ce46c9ac5347e38
ortutay <marcell.ortutay@gmail.com>
2025-01-29 15:30:01 -0800

    x

--- src/step/PageActionStep.js
+++ src/step/PageActionStep.js
@@ -9,8 +9,7 @@ export const PageActionStep = class extends BaseStep {
     this.actions = [
       new PageAction(...),
       new PageAction(...),
-    ]
-
+    ];
   }
 
   async finish(cursor) {
@@ -18,8 +17,6 @@ export const PageActionStep = class extends BaseStep {
   }
 
   async process({ cursor, item, index }, cb) {
-
-
     const url = 'https://abc...'; // get url from item....
     const fetcher = cursor.ctx.fetcher;
 
@@ -33,29 +30,5 @@ export const PageActionStep = class extends BaseStep {
       // -- or --
       cb(doc);
     }
-
-    // logger.info(`Fetch step for ${item}`);
-    // const options = { multiple: true, priority: index };
-
-    // if (this.waitForText) options.waitForText = this.waitForText;
-    // if (this.maxPages) options.maxPages = this.maxPages;
-    // if (this.active) options.active = this.active;
-    // if (this.css) options.css = this.css;
-
-    // const streams = [];
-    // for (const field of this.urlFields) {
-    //   const url = item[field];
-    //   if (!url) continue;
-    //   const stream = await cursor.ctx.fetcher.fetch(url, options);
-    //   streams.push(stream);
-    // }
-
-    // // TODO: race the streams instead of iterating
-    // for (const stream of streams) {
-    //   for await (const doc of stream) {
-    //     logger.info(`Fetch step yielding ${doc}`);
-    //     cb(new Item({}, doc));
-    //   }
-    // }
   }
 }
