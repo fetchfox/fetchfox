281ba3c6b04ce2c22130dc7221917684729db6eb
ortutay <marcell.ortutay@gmail.com>
2025-02-20 12:19:49 -0800

    apply global limit to all steps

--- src/workflow/Workflow.js
+++ src/workflow/Workflow.js
@@ -122,10 +122,10 @@ export const Workflow = class extends BaseWorkflow {
     this.cursor = new Cursor(this.ctx, this.steps, cb);
     const last = this.steps[this.steps.length - 1];
 
-    let originalLimit = last.limit;
-
     if (this.ctx.limit) {
-      last.limit = last.limit ? Math.min(this.ctx.limit, last.limit) : this.ctx.limit;
+      for (const step of this.steps) {
+        step.limit = step.limit ? Math.min(this.ctx.limit, step.limit) : this.ctx.limit;
+      }
     }
 
     const msg = ` Starting workflow with ${this.steps.length} steps: ${this.steps.map(s => (''+s).replace('Step', '')).join(' -> ')} `;
@@ -139,7 +139,6 @@ export const Workflow = class extends BaseWorkflow {
       return this.cursor.out(true);
 
     } finally {
-      last.limit = originalLimit;
       this.cursor.finishAll();
 
       if (this.controller) {
