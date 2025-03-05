36a36879d10d3182b428b20ce9fd025d8eb216b0
Sanchit Dass <sanchitd9@gmail.com>
2025-03-05 12:57:48 -0500

    youtube export

--- src/media/MediaExporter.js
+++ src/media/MediaExporter.js
@@ -1,10 +1,12 @@
 import { logger } from '../log/logger.js';
 import { srid } from '../util.js';
 import { S3Helper } from '../s3/s3.js';
+import ytdl from '@distube/ytdl-core';
 
 export const MediaExporter = class {
   constructor(options) {
     this.allowedTypes = ['application/pdf', 'image/'];
+    this.videoTypes = ['youtube'];
     this.s3 = new S3Helper(options);
   }
 
@@ -22,15 +24,19 @@ export const MediaExporter = class {
     const mediaUrl = new URL(url);
     const contentType = await this._getContentType(mediaUrl);
 
-    if (this.allowedTypes.some(type => contentType.startsWith(type))) {
-      console.log('Allowed');
-    } else {
+    const supportedType = this.allowedTypes.some(type => contentType.startsWith(type));
+    const supportedVideo = this.videoTypes.some(type => mediaUrl.hostname.includes(type));
+
+    if (!(supportedType || supportedVideo)) {
       logger.error(`${contentType} is not supported.`);
+      return '';
     }
 
+    let s3Url;
     const fileName = mediaUrl.pathname.split('/').pop();
 
-    const s3Url = await this.generalMediaExporter(mediaUrl, fileName, contentType);
+    if (supportedType) s3Url = await this.generalMediaExporter(mediaUrl, fileName, contentType);
+    if (supportedVideo) s3Url = await this.youtubeExporter(mediaUrl, fileName);
 
     logger.info(`Exported ${fileName}: ${s3Url}`);
 
@@ -48,8 +54,21 @@ export const MediaExporter = class {
     const buffer = await resp.arrayBuffer();
 
     const id = srid();
-    const objectKey = `export/media/${id}/${fileName}`;
+    const key = `export/media/${id}/${fileName}`;
 
-    return this.s3.put(objectKey, buffer, contentType);
+    return this.s3.put(key, buffer, contentType);
+  }
+
+  async youtubeExporter(url, fileName) {
+    const stream = ytdl(url.href, { quality: 'highestvideo' });
+
+    const chunks = [];
+    for await (const chunk of stream) chunks.push(chunk);
+    const buffer = Buffer.concat(chunks);
+
+    const id = srid();
+    const key = `export/youtube/${id}/${fileName}.mp4`;
+
+    return this.s3.put(key, buffer, 'video/mp4');
   }
 };
