6cca76a8edfb481047adfa8c0adaa767819fde4d
Sanchit Dass <sanchitd9@gmail.com>
2025-03-05 13:21:25 -0500

    tests

--- test/media/Media.test.js
+++ test/media/Media.test.js
@@ -4,12 +4,29 @@ import { setTestTimeout } from '../lib/util.js';
 
 describe('Export Media', function () {
 
+  // Media exports take time
   setTestTimeout(this, 10 * 1000);
 
   it('should export pdf', async () => {
-    const pdfUrl = 'https://arxiv.org/pdf/2401.14196';
+    const url = 'https://arxiv.org/pdf/2401.14196';
     const exporter = new MediaExporter();
-    const s3Url = await exporter.export(pdfUrl);
+    const s3Url = await exporter.export(url);
+
+    assert.ok(s3Url, 'export url should not be empty');
+  });
+
+  it('should export image', async () => {
+    const url = 'https://upload.wikimedia.org/wikipedia/commons/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg';
+    const exporter = new MediaExporter();
+    const s3Url = await exporter.export(url);
+
+    assert.ok(s3Url, 'export url should not be empty');
+  });
+
+  it('should export youtube video', async () => {
+    const url = 'https://www.youtube.com/shorts/N1934B5vaGI';
+    const exporter = new MediaExporter();
+    const s3Url = await exporter.export(url);
 
     assert.ok(s3Url, 'export url should not be empty');
   });
