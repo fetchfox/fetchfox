a7cef40df8597ec240677ccda0ffc57dcf515eaa
ortutay <marcell.ortutay@gmail.com>
2025-03-27 14:05:22 -0700

    clean up benchmark

--- benchmark/extract/ct.curaleaf.com-extract.bench.js
+++ benchmark/extract/ct.curaleaf.com-extract.bench.js
@@ -6,55 +6,6 @@ import { checkItemsAI } from '../lib/checks.js';
 describe('extract ct.curaleaf.com', async function() {
   const matrix = standardMatrix();
 
-  // const cases = [
-  //   {
-  //     url: 'https://ct.curaleaf.com/shop/connecticut/curaleaf-ct-stamford/products/6707112fe6c31500014349ef',
-  //     expected: {
-  //       name: 'Curaleaf Whole Flower Raspberry P (S) 00169',
-  //       price: '$34',
-  //       weight_oz: '1/8oz',
-  //       weight_grams: '3.5g',
-  //       strength: '15.27% Total THC',
-  //     },
-  //   },
-
-  //   {
-  //     url: 'https://ct.curaleaf.com/shop/connecticut/curaleaf-ct-stamford/products/67cb3831ca2e3669ed31421e',
-  //     expected: {
-  //       name: 'Curaleaf Whole Flower Ivory (H) 00533',
-  //       price: '$34',
-  //       weight_oz: '1/8oz',
-  //       weight_grams: '3.5g',
-  //       strength: '26.04% Total THC',
-  //     },
-  //   },
-
-  //   {
-  //     url: 'https://ct.curaleaf.com/shop/connecticut/curaleaf-ct-stamford/products/674f41cb1f816bc40fa34eed',
-  //     expected: {
-  //       name: 'Curaleaf Whole Flower Indica Princess (I) 00246',
-  //       price: '$32',
-  //       weight_oz: '1/8oz',
-  //       weight_grams: '3.5g',
-  //       strength: '18.98% Total THC',
-  //     },
-  //   },
-  // ];
-
-  // for (const { url, expected } of cases) {
-  //   const wf = await fox
-  //     .init(url)
-  //     .extract({
-  //       questions: {
-  //         name: 'Name of the product',
-  //         price: 'Price of the product. Format: $XX',
-  //         weight_oz: 'Weight in oz',
-  //         weight_grams: 'Weight in g. Format: XX.Xg',
-  //         strength: 'Strength % THC',
-  //       },
-  //       mode: 'single',
-  //     });
-      
   const expected = [
     {
       product_name: 'Curaleaf Whole Flower A Tartz (H) 00522',
@@ -235,11 +186,11 @@ describe('extract ct.curaleaf.com', async function() {
   ];
 
   const cases = [
-    // {
-    //   name: 'live',
-    //   url: 'https://ct.curaleaf.com/shop/connecticut/curaleaf-ct-stamford/categories/flower',
-    //   expected,
-    // },
+    {
+      name: 'live',
+      url: 'https://ct.curaleaf.com/shop/connecticut/curaleaf-ct-stamford/categories/flower',
+      expected,
+    },
     {
       name: 'saved',
       url: 'https://ffcloud.s3.us-west-2.amazonaws.com/fetchfox-docs/93ue3wfv78/https-ct-curaleaf-com-shop-connecticut-curaleaf-ct-stamford-categories-flower.html',
