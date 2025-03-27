56369ca2b6cb6bbce35d7e07e775f0a1cc2eff34
ortutay <marcell.ortutay@gmail.com>
2025-03-27 13:58:31 -0700

    clean up pokemon benchmark

--- benchmark/extract/pokemondb.net-extract.bench.js
+++ benchmark/extract/pokemondb.net-extract.bench.js
@@ -11,36 +11,31 @@ describe('extract pokemondb.net', async function() {
     {
       name: 'Bulbasaur',
       number: '#0001',
-      url: 'https://pokemondb.net/pokedex/bulbasaur',
     },
     {
       name: 'Ivysaur',
       number: '#0002',
-      url: 'https://pokemondb.net/pokedex/ivysaur',
     },
     {
       name: 'Venusaur',
       number: '#0003',
-      url: 'https://pokemondb.net/pokedex/venusaur',
     },
     {
       name: 'Charmander',
       number: '#0004',
-      url: 'https://pokemondb.net/pokedex/charmander',
     },
     {
       name: 'Charmeleon',
       number: '#0005',
-      url: 'https://pokemondb.net/pokedex/charmeleon',
     },
   ];
 
   const cases = [
-    // {
-    //   name: 'live',
-    //   url: 'https://pokemondb.net/pokedex/national',
-    //   expected,
-    // },
+    {
+      name: 'live',
+      url: 'https://pokemondb.net/pokedex/national',
+      expected,
+    },
     {
       name: 'saved',
       url: 'https://ffcloud.s3.us-west-2.amazonaws.com/fetchfox-docs/djh6e69cux/https-pokemondb-net-pokedex-national.html',
@@ -51,7 +46,6 @@ describe('extract pokemondb.net', async function() {
   const questions = {
     name:	'What is the name of this pokemon?',
     number: 'What is the pokemon number',
-    url: `What is the URL of the pokemon`,
     description: `Describe this pokemon in 50-100 words`,
   }
 
